cb097b0094550230b4f7b9758f348d04d7730327
Drayton-Dobbs <jamesdraytondobbs@gmail.com>
2018-11-28 11:28:01 -0600

    added main components

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <h1>Sign up page</h1>;
+    }
+}
+
+export default SignUp;
---
+++ client/src/components/TrainerPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Trainers extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <h1>get swole</h1>;
+    }
+}
+
+export default Trainers;
